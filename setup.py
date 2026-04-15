d4ac275f1b91ed619107f3c7d0b88304b6af75ca
pradhyumna91 <pradhyumnakiledar91@gmail.com>
2026-04-15 17:20:56 -0400

    Commiting with some updated changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package',
+    version='0.1.0',
+    author='Pradhyumna Kiledar',
+    author_email='pradhyumnakiledar91@gmail.com'
+    packages=find_packages(),
+    )
