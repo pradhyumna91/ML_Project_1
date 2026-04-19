a3bfa9cff278468dd9b7d2f30d353b4fb7d03b3c
pradhyumna91 <pradhyumnakiledar91@gmail.com>
2026-04-19 18:49:41 -0400

    Save before push

--- src/exception.py
+++ src/exception.py
@@ -5,4 +5,13 @@ def error_message_detail(error, error_detail: sys):
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     error_message = f"Error occurred in {file_name} at line {line_number}: {str(error)}"
+    file_name,exc_tb.tb_lineno, str(error)
     return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
